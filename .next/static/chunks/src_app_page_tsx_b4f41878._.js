c2198c96674254b7ea064fa4f43704584de03be4
LinhNguyen2005 <baolinh2911@gmail.com>
2025-06-28 09:56:17 +0700

    Add .next/static/chunks/src_app_page_tsx_b4f41878._.js 1751079374917

---
+++ .next/static/chunks/src_app_page_tsx_b4f41878._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_f3cba209._.js",
+  "static/chunks/src_02c3edb7._.js"
+],
+    source: "dynamic"
+});
